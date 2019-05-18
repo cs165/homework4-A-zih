88b1b1f233f5c085ce408617b2177dd7f1f6b5d7
A-zih <44132730+A-zih@users.noreply.github.com>
2019-05-18 20:32:18 +0800

    Add files via upload

--- menu-screen.js
+++ menu-screen.js
@@ -2,9 +2,36 @@
 // the music visualizer.
 //
 // See HW4 writeup for more hints and details.
+var s = document.getElementById('song-selector');
+var t = document.getElementById('query-input');
+var e = document.getElementById('error');
+var theme = ['candy', 'charlie brown', 'computers', 'dance', 'donuts', 'hello kitty', 'flowers', 'nature', 'turtles', 'space'];
 class MenuScreen {
-  constructor() {
-    // TODO(you): Implement the constructor and add fields as necessary.
+  constructor(containerElement) {
+    this.containerElement = containerElement;
+
+    // song
+    fetch('https://fullstackccu.github.io/homeworks/hw4/songs.json')
+      .then(onResponse)
+      .then(onDataReady);
+
+    //theme
+    
+    t.value = theme[Math.floor(Math.random()*10)];
+
+    //submit
+    this.goBtn = document.getElementById("go");
+    this.goBtn = addEventListener('submit',this.onClick);
   }
-  // TODO(you): Add methods as necessary.
+  onClick(event){
+    event.preventDefault();
+    url = "http://api.giphy.com/v1/gifs/search?q="+ t.value +"&api_key=VZ7FFJUrfrJrkWSEGvKsVtBp6wNRiyxZ";
+    app.music.gif.fetch_gif(url);
+  }
+  show(){
+    this.containerElement.classList.remove('inactive');
+  }
+  hide() {
+    this.containerElement.classList.add('inactive');
+  }
 }
